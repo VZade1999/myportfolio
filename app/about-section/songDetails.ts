9d6d12ad4203cbaf2db756e8d1b158e84160a5f4
Vaibhav Zade <vaibhavz@incubxperts.com>
2024-06-23 23:49:23 +0530

    added public folder

--- app/about-section/songDetails.ts
+++ app/about-section/songDetails.ts
@@ -18,60 +18,60 @@ import song10Image from './.././../public/songs/10.png';
 
 
 export const songs = [
-  // {
-  //   title: "Gulabi Sadi",
-  //   artist: "Sanju Rathod",
-  //   image: song1Image,
-  //   link: "https://open.spotify.com/track/4ITtnvLQ6SOA8XhiqkLa24",
-  // },
-  // {
-  //   title: "Galavar Khali",
-  //   artist: "Swapnil Bandodkar",
-  //   image: song2Image,
-  //   link: "https://open.spotify.com/track/2VhBp6Q0YPBFlDR1CuMWNp",
-  // },
-  // {
-  //   title: "Aarambh Hai Prachan",
-  //   artist: "Piyush Mishra",
-  //   image: song3Image,
-  //   link: "https://open.spotify.com/track/1PZZtXR7nsNIyRcqd7UeiF",
-  // },
-  // {
-  //   title: "Sang Rahiyo",
-  //   artist: "Jasleen Royal",
-  //   image: song4Image,
-  //   link: "https://open.spotify.com/track/45PA9lW7U5LYnlpGrdTyHY",
-  // },
-  // {
-  //   title: "Tere Hawaale",
-  //   artist: "Laal Singh Chaddha",
-  //   image: song5Image,
-  //   link: "https://open.spotify.com/track/3RorJ5DnxrT70LuRfhLn3t",
-  // },
-  // {
-  //   title: "2002",
-  //   artist: "Anne Marie",
-  //   image: song6Image,
-  //   link: "https://open.spotify.com/track/2BgEsaKNfHUdlh97KmvFyo",
-  // },
-  // {
-  //   title: "Mann Ki Lagan",
-  //   artist: "Rahat Fatech Ali Khan",
-  //   image: song7Image,
-  //   link: "https://open.spotify.com/track/0s8fGVzyn6URNrUVwCE3dV",
-  // },
-  // {
-  //   title: "Bad ass",
-  //   artist: "SL, Knucks",
-  //   image: song8Image,
-  //   link: "https://open.spotify.com/track/32qnBgfzURbwNayQIEB5W5?si=513f3ecbfc2e42df",
-  // },
-  // {
-  //   title: "Unstoppable",
-  //   artist: "Sia",
-  //   image: song9Image,
-  //   link: "https://open.spotify.com/track/2J2Z1SkXYghSajLibnQHOa",
-  // },
+  {
+    title: "Gulabi Sadi",
+    artist: "Sanju Rathod",
+    image: song1Image,
+    link: "https://open.spotify.com/track/4ITtnvLQ6SOA8XhiqkLa24",
+  },
+  {
+    title: "Galavar Khali",
+    artist: "Swapnil Bandodkar",
+    image: song2Image,
+    link: "https://open.spotify.com/track/2VhBp6Q0YPBFlDR1CuMWNp",
+  },
+  {
+    title: "Aarambh Hai Prachan",
+    artist: "Piyush Mishra",
+    image: song3Image,
+    link: "https://open.spotify.com/track/1PZZtXR7nsNIyRcqd7UeiF",
+  },
+  {
+    title: "Sang Rahiyo",
+    artist: "Jasleen Royal",
+    image: song4Image,
+    link: "https://open.spotify.com/track/45PA9lW7U5LYnlpGrdTyHY",
+  },
+  {
+    title: "Tere Hawaale",
+    artist: "Laal Singh Chaddha",
+    image: song5Image,
+    link: "https://open.spotify.com/track/3RorJ5DnxrT70LuRfhLn3t",
+  },
+  {
+    title: "2002",
+    artist: "Anne Marie",
+    image: song6Image,
+    link: "https://open.spotify.com/track/2BgEsaKNfHUdlh97KmvFyo",
+  },
+  {
+    title: "Mann Ki Lagan",
+    artist: "Rahat Fatech Ali Khan",
+    image: song7Image,
+    link: "https://open.spotify.com/track/0s8fGVzyn6URNrUVwCE3dV",
+  },
+  {
+    title: "Bad ass",
+    artist: "SL, Knucks",
+    image: song8Image,
+    link: "https://open.spotify.com/track/32qnBgfzURbwNayQIEB5W5?si=513f3ecbfc2e42df",
+  },
+  {
+    title: "Unstoppable",
+    artist: "Sia",
+    image: song9Image,
+    link: "https://open.spotify.com/track/2J2Z1SkXYghSajLibnQHOa",
+  },
   {
     title: "pehel bhi me",
     artist: "Unknown T",

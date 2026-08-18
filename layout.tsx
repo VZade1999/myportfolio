8e6f93d1664b41449262be2874db991f2c88ee6e
Vaibhav Zade <97147339+VZade1999@users.noreply.github.com>
2026-08-18 07:08:00 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,84 @@
+import "./globals.css";
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Syne } from "@next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
+const syne = Syne({
+  subsets: ["latin"],
+  display: "block",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "Vaibhav Zade",
+  description:
+    "Senior Software Engineer | Full Stack Developer (React, Next.js, Node.js) | AI/LLM Integrations (LangChain, RAG) | 5+ Years Experience",
+  generator: "Next.js",
+  applicationName: "Vaibhav Zade",
+  keywords: [
+    "developer",
+    "full stack developer",
+    "senior software engineer",
+    "react",
+    "next.js",
+    "node.js",
+    "typescript",
+    "aws",
+    "ai engineer",
+    "langchain",
+    "rag",
+    "software developer",
+    "portfolio",
+    "full stack developer portfolio",
+  ],
+  colorScheme: "dark",
+  openGraph: {
+    title: "Vaibhav Zade",
+    description:
+      "Senior Software Engineer | Full Stack Developer (React, Next.js, Node.js) | AI/LLM Integrations (LangChain, RAG) | 5+ Years Experience",
+    url: "https://portfolio-ar.vercel.app/",
+    siteName: "www.vaibhavzade.com",
+    images: [
+      {
+        url: "https://github.com/VZade1999/myportfolio/blob/main/public/profiless.png?raw=true",
+        width: 1200,
+        height: 630,
+        alt: "vaibhav — MERN Engineer",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  category: "technology",
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body
+        className={`${syne.className} scroll-smooth scrollbar-thin scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+      >
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
